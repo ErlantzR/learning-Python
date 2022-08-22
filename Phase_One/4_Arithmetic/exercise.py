1788b210f50bc0ef1dbb5497fe7dcbebe2f4aba6
Erlantz <erlantzram@gmail.com>
2022-08-22 15:41:24 +0100

    chapter 4 completed

---
+++ Phase_One/4_Arithmetic/exercise.py
@@ -0,0 +1,13 @@
+# I have 50 apples and eat tweenty-three
+apples = 50 - 23
+print(apples)
+# minutes in 2022
+minutes2022 = 365 * 24 * 60
+print(minutes2022)
+# 32452 herring to be group into tanks of 13 herring each
+# how many tanks I need
+tanks = (32452 // 13) + 1
+print(tanks)
+# herring in tank which doesn't have 13 herring
+herring = 32452 % 13
+print(herring)
