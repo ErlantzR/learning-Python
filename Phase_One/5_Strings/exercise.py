55d2e88edce6f83045af5bcad2cc20b0eaf3ec2a
Erlantz <erlantzram@gmail.com>
2022-08-22 16:39:03 +0100

    completed challenge chapter 5

--- Phase_One/5_Strings/exercise.py
+++ Phase_One/5_Strings/exercise.py
@@ -5,3 +5,14 @@ count = 5
 thing = "directories"
 print(f"There are {count} {thing}.")
 print("There are %d %s." % (count, thing))
+
+# challenge
+# Make this program print:
+# One Sunday morning the warm sun came up and - pop!
+day = "Sunday"
+time_of_day = "morning"
+star_quality = "warm"
+star_name = "sun"
+star_direction = "up"
+mysterious_sound = "pop"
+print(f"One {day} {time_of_day} the {star_quality} {star_name} came {star_direction} and - {mysterious_sound}!")
