5ae39849bec57cb93b7240c8836d4731ad61a712
Erlantz <erlantzram@gmail.com>
2022-08-22 16:20:15 +0100

    chapter 5 completed

---
+++ Phase_One/5_Strings/exercise.py
@@ -0,0 +1,7 @@
+animal = "Frog"
+print(f"Hello to the {animal}!")
+print("Hello to the %s!" % animal)
+count = 5
+thing = "directories"
+print(f"There are {count} {thing}.")
+print("There are %d %s." % (count, thing))
