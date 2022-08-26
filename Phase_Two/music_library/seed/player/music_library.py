e41544b9dbe6c869e712c75a0052f82165f5cb4a
Erlantz <erlantzram@gmail.com>
2022-08-26 09:37:34 +0100

    added music library exercise template with first exercisse done: create backend using TDD

---
+++ Phase_Two/music_library/seed/player/music_library.py
@@ -0,0 +1,16 @@
+class MusicLibrary:
+    def __init__(self):
+        self.songs_list = []
+
+    def all(self):
+        return self.songs_list
+
+    def add(self, song_name):
+        self.songs_list.append(song_name)
+
+    def remove(self, song_index):
+        if song_index + 1 <= len(self.songs_list):
+            self.songs_list.pop(song_index)
+            return True
+        
+        return False
---
+++ Phase_Two/music_library/seed/tests/test_music_library.py
@@ -0,0 +1,61 @@
+import unittest
+
+from player.music_library import MusicLibrary
+
+
+class TestMusicLibrary(unittest.TestCase):
+    def test_constructs(self):
+        MusicLibrary()
+
+    def test_all_method_exists(self):
+        MusicLibrary().all()
+
+    def test_songs_list_empty_when_created(self):
+        library = MusicLibrary()
+        self.assertEqual(library.all(), [])
+
+    def test_add_method_exists(self):
+        MusicLibrary().add("Test song")
+
+    def test_add_a_song_to_list(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        self.assertEqual(library.all(), ["Test song"])
+    
+    def test_add_a_second_song_to_list(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        library.add("Second test song")
+        self.assertEqual(library.all(), ["Test song", "Second test song"])
+
+    def test_remove_method_exists(self):
+        MusicLibrary().remove(1)
+
+    def test_remove_a_song(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        library.add("Second test song")
+        self.assertEqual(len(library.all()), 2)
+        library.remove(1)
+        self.assertEqual(len(library.all()), 1)
+
+    def test_remove_an_specific_song(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        library.add("Second test song")
+        library.remove(1)
+        self.assertEqual(library.all(), ["Test song"])
+        library.add("Second test song")
+        library.remove(0)
+        self.assertEqual(library.all(), ["Second test song"])
+
+    def test_remove_returns_true_when_successfuly_removed(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        self.assertTrue(library.remove(0))
+
+    def test_remove_returns_false_when_index_not_in_list(self):
+        library = MusicLibrary()
+        library.add("Test song")
+        self.assertFalse(library.remove(2))
+
